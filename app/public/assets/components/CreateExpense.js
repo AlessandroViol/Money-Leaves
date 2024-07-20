124f74c9cbc1d24143848554894e21a915e87b2c
AlessandroViol <viol.alessandro97@gmail.com>
2024-07-20 16:44:59 +0200

    Implemented basic expense creation form

--- app/public/assets/components/CreateExpense.js
+++ app/public/assets/components/CreateExpense.js
@@ -26,12 +26,54 @@ const CreateExpense = {
                   type="number" 
                   class="form-control" 
                   aria-label="Euros amount (with dot and two decimal places)" 
-                  v-model="total_cost" 
+                  v-model.number="total_cost"
                   min="0" max="2000" 
-                  :formatter="moneyFormat"
                 >
                 <span class="input-group-text" id="total_cost">€</span>
               </div>
+
+              <label for="category" class="form-label">Category</label>
+							<select class="form-select mb-3" v-model="selected_category" aria-label="Default select example">
+                <option disabled value="">Select a category</option>
+                <option v-for="category in categories" :value="category">{{category}}</option>
+              </select>
+
+              <label for="date" class="form-label">Date</label>
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" aria-label="Expense date" aria-describedby="expense-date" :value="date_string" readonly>
+                <div class="btn-group">
+                  <button id="filter-calendar" class="btn btn-sm btn-outline-secondary dropdown-toggle d-flex align-items-center rounded-0 rounded-end gap-1" 
+                    type="button" data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false" 
+                  >
+                    <svg class="bi">
+                      <use xlink:href="#calendar3" />
+                    </svg>
+                  </button>
+                  <div class="dropdown-menu dropdown-menu-end py-0 border-0">
+                    <calendar @updateDate="updateDate"></calendar>
+                  </div>
+                </div>
+              </div>
+
+              <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" id="description" rows="3" v-model="description" placeholder="Expense's description"></textarea>
+              </div>
+
+              <div class="bg-body-tertiary px-4 rounded-3">
+                <header>
+                  <div class="small opacity-50 py-2 d-flex">
+                    <span class="col">Username</span>
+                    <span class="col text-end">Quota</span>
+                  </div>
+                </header>
+                <div class="row py-2 d-flex flex-row border-top" v-for="contributor in contributors">
+                  <span class="col">@{{ contributor.user_id }}</span>
+                  <span class="col text-end" :class="{'text-danger': contributor.quota<0}">{{ contributor.quota.toFixed(2) }} €</span>
+                </div>
+                
+              </div>
+
 						</div>
 					</section>
 				</div>
@@ -43,7 +85,25 @@ const CreateExpense = {
 		return {
 			username: '',
 
-			total_cost: 0,
+			total_cost: 0.0,
+			categories: [
+				'Refound',
+				'Food and beverages',
+				'Entertainment',
+				'Taxes and bills',
+				'Transportation',
+				'Travel',
+				'Present',
+			],
+			selected_category: '',
+			selected_date: {
+				day: new Date().getDate(),
+				month: new Date().getMonth(),
+				year: new Date().getFullYear(),
+			},
+			description: '',
+
+			contributors: [],
 
 			query: '',
 			userList: [],
@@ -51,23 +111,26 @@ const CreateExpense = {
 	},
 
 	methods: {
-		moneyFormat(value) {
-			return value.tofixed(2);
-		},
-
 		goToSignin() {
 			this.$router.push({ path: '/signin' });
+		},
+
+		updateDate(date) {
+			console.log('Date', date);
+			this.selected_date = date;
+		},
+	},
+
+	computed: {
+		date_string() {
+			return `${this.selected_date.day}/${this.selected_date.month}/${this.selected_date.year}`;
 		},
 	},
 
 	watch: {
 		total_cost(value) {
-			console.log('wtf');
-			if (value > 2000) {
-				value = 2000;
-			} else if (value < 0) {
-				value = 0;
-			}
+			this.contributors[0].quota =
+				value - this.contributors.reduce((sumOfQuotas, { quota }) => sumOfQuotas + quota, 0) + this.contributors[0].quota;
 		},
 
 		async query(value) {
@@ -129,6 +192,7 @@ const CreateExpense = {
 		if (response.ok) {
 			const res = await response.json();
 			this.username = res.username;
+			this.contributors.push({ user_id: res.username, quota: 0 });
 		}
 	},
 };
