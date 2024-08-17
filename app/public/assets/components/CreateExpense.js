8b2902febe8a8cf78164becb8ba6bd8921a7d652
AlessandroViol <viol.alessandro97@gmail.com>
2024-08-17 11:57:04 +0200

    Expense creation form

--- app/public/assets/components/CreateExpense.js
+++ app/public/assets/components/CreateExpense.js
@@ -60,7 +60,7 @@ const CreateExpense = {
                 <textarea class="form-control" id="description" rows="3" v-model="description" placeholder="Expense's description"></textarea>
               </div>
 
-              <div class="bg-body-tertiary px-4 rounded-3">
+              <div class="bg-body-tertiary px-4 pb-2 rounded-3">
                 <header>
                   <div class="small opacity-50 py-2 d-flex">
                     <span class="col">Username</span>
@@ -68,14 +68,103 @@ const CreateExpense = {
                   </div>
                 </header>
                 <div class="row py-2 d-flex flex-row border-top" v-for="contributor in contributors">
-                  <span class="col">@{{ contributor.user_id }}</span>
-                  <span class="col text-end" :class="{'text-danger': contributor.quota<0}">{{ contributor.quota.toFixed(2) }} €</span>
+                  <span class="col" :class="{'text-primary fw-medium': isCurrentUser(contributor.user_id)}">
+										@{{ contributor.user_id }}
+									</span>
+                  <span class="col text-end" >
+										<span :class="{'text-danger': contributor.quota<0}" v-if="isCurrentUser(contributor.user_id)">
+											{{ contributor.quota.toFixed(2) }} €
+										</span>
+										<span v-if="!isCurrentUser(contributor.user_id)">
+											<span class="input-group mb-3">
+												<input 
+													type="number" 
+													class="form-control" 
+													aria-label="Euros amount (with dot and two decimal places)" 
+													v-model.number="contributor.quota"
+													min="-2000" max="2000" 
+												>
+												<span class="input-group-text" id="quota">€</span>
+											</span>
+											<a class="link-danger" href="#" @click.prevent="removeContributor(contributor.user_id)">
+												<span class="material-symbols-outlined align-middle" style="font-size:1rem;">cancel</span>
+											</a>
+										</span>
+									</span>
                 </div>
                 
+								<div class="mt-4">
+									<div class="mb-3">
+										<button
+											class="btn btn-outline-secondary btn-lg px-3 w-100"
+											type="button"
+											data-bs-toggle="collapse"
+											data-bs-target="#expenseSearch"
+											aria-controls="expenseSearch"
+											aria-expanded="false"
+											aria-label="Toggle search"
+											@click="collapseSearch"
+										>
+											<span class="material-symbols-outlined align-middle">
+												person_add
+											</span>
+										</button>
+									</div>
+
+									<div id="expenseSearch" class="w-100 collapse px-2 mb-3">
+										<div class="shadow mb-2">
+											<input class="py-3 rounded bg-body-secondary form-control" type="text" placeholder="Search" aria-label="Search" v-model="query"/>
+										</div>
+
+										<div class="px-3">
+											<div v-for="user in userList">
+												<div class="row py-2 d-flex flex-row border-bottom" v-if="user._id !== username">
+													<span class="col">
+														@{{ user._id }}
+													</span>
+													<span class="col">
+														{{ user.name }}
+													</span>
+													<span class="col">
+														{{ user.surname }}
+													</span>
+													<button class="btn btn-sm btn-primary col" @click="addContributor(user._id)" :disabled="isContributor(user._id)">Select</button>
+												</div>
+											</div>
+										</div>
+									</div>
+
+								</div>
               </div>
 
 						</div>
+						
+
+						<div>
+							<button type="button" class="btn btn-sm btn-primary" @click="confirmExpense">Create Expense</button>
+						</div>
+
+						<div class="modal fade" id="createExpenseConfirm" tabindex="-1" aria-labelledby="createExpenseConfirmLabel" aria-hidden="true">
+							<div class="modal-dialog modal-dialog-centered">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h1 class="modal-title fs-5" id="createExpenseConfirmLabel">Create Expense</h1>
+										<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+									</div>
+									<div class="modal-body">
+										<expense :expense="expense" :username="username"></expense> 
+									</div>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-primary mx-auto" >Yes</button>
+										<button type="button" class="btn btn-secondary mx-auto" data-bs-dismiss="modal">Cancel</button>
+									</div>
+								</div>
+							</div>
+						</div>
+
+
 					</section>
+
 				</div>
 			</div>
 		</section>
@@ -107,6 +196,8 @@ const CreateExpense = {
 
 			query: '',
 			userList: [],
+
+			expense: { total_cost: 0 },
 		};
 	},
 
@@ -118,6 +209,45 @@ const CreateExpense = {
 		updateDate(date) {
 			console.log('Date', date);
 			this.selected_date = date;
+		},
+
+		isCurrentUser(user_id) {
+			return user_id === this.username;
+		},
+
+		isContributor(user_id) {
+			return this.contributors.find((contributor) => contributor.user_id === user_id);
+		},
+
+		addContributor(user_id) {
+			if (!this.isContributor(user_id)) {
+				this.contributors.push({ user_id, quota: 0 });
+			}
+		},
+
+		removeContributor(user_id) {
+			this.contributors = this.contributors.filter((contributor) => contributor.user_id !== user_id);
+		},
+
+		collapseSearch() {
+			this.userList = [];
+			this.query = '';
+		},
+
+		confirmExpense() {
+			this.expense = {
+				payer_id: this.username,
+				total_cost: this.total_cost,
+				description: this.description,
+				category: this.selected_category,
+				date: this.selected_date,
+				contributors: this.contributors,
+			};
+
+			console.log('Expense created: ', this.expense);
+
+			const modal = new bootstrap.Modal(document.getElementById(`createExpenseConfirm`));
+			modal.show();
 		},
 	},
 
